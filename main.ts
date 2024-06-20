a3336f70fddc8096f5d30e4cdbff97243b96c758
TheseusGrey <TheseusGrey@proton.me>
2024-06-21 00:58:30 +0100

    Okay, cleaned out all the sample stuff we didn't need and got it all organised

--- main.ts
+++ main.ts
@@ -1,118 +1,76 @@
-import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { findNvim, attach, } from 'neovim'
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { findNvim, } from 'neovim'
 import * as child_process from 'node:child_process'
+import { NvimVersion } from 'neovim/lib/utils/findNvim';
 
-// Remember to rename these classes and interfaces!
 
 interface EditInNeovimSettings {
-	mySetting: string;
 	terminal: string;
+	listenOn: string;
+	openNeovimOnLoad: boolean;
 }
 
 const DEFAULT_SETTINGS: EditInNeovimSettings = {
-	mySetting: 'default',
-	terminal: process.env.TERMINAL || "alacritty",
+	terminal: process.env.TERMINAL || "xterm",
+	listenOn: "127.0.0.1:2006",
+	openNeovimOnLoad: true,
 }
 
 export default class EditInNeovim extends Plugin {
 	settings: EditInNeovimSettings;
 
+	spawnNewInstanceOnLoad(nvim: NvimVersion, adapter: FileSystemAdapter) {
+		child_process.spawn(this.settings.terminal, [
+			'-e',
+			nvim.path,
+			"--listen",
+			this.settings.listenOn,
+		], { cwd: adapter.getBasePath() })
+	}
+
+	openInNeovimInstance(file: TFile | null) {
+		if (!file) return;
+		if (!(this.app.vault.adapter instanceof FileSystemAdapter)) return;
+
+		const found = findNvim({ orderBy: 'desc', minVersion: '0.9.0' });
+
+		console.log(`Opening ${this.app.vault.adapter.getFullPath(file.path)} in neovim`)
+		child_process.spawn(found.matches[0].path, [
+			'--server', this.settings.listenOn,
+			'--remote',
+			`${file.path}`
+		])
+	}
+
 	async onload() {
 		await this.loadSettings();
 		const adapter = this.app.vault.adapter;
-		console.log("Edit in Neovim Loaded!")
+		console.log("Edit in Neovim Loaded! listening on: " + this.settings.listenOn)
 		const found = findNvim({ orderBy: 'desc', minVersion: '0.9.0' })
+		if (found.matches.length === 0) throw Error("No Valid nvim binaries :'( plugin can't run without them");
 
-		if (!(adapter instanceof FileSystemAdapter)) return;
-		child_process.spawn(this.settings.terminal, [
-			'-e',
-			found.matches[0].path,
-			"--listen",
-			"127.0.0.1:6000",
-		], { cwd: adapter.getBasePath() })
+		if (!(adapter instanceof FileSystemAdapter)) throw Error("I need a FileSystemAdapter in order to work, are you running on mobile?");
 
-		this.app.workspace.on("file-open", file => {
-			if (!file) return;
-			console.log(`Opening ${adapter.getFullPath(file.path)} in neovim`)
-			child_process.spawn(found.matches[0].path, [
-				'--embed',
-				'--server', '127.0.0.1:6000',
-				'--remote',
-				`${file.path}`
-			])
-		})
+		if (this.settings.openNeovimOnLoad) this.spawnNewInstanceOnLoad(found.matches[0], adapter)
 
-		//const nvim_proc = child_process.spawn(found.matches[0].path, ['--clean', '--embed', '--listen', '127.0.0.1:6000'], {});
-
-		// This command is how we "push" a new buffer to an instance running using --listen
-		// "$nvim_exec" --server "$server_path" --remote-send "<C-\><C-n>:n $1<CR>:call cursor($2)<CR>"
-		//const nvim = attach({ proc: nvim_proc });
-
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.app.workspace.on("file-open", this.openInNeovimInstance)
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new EditInNeovimSettingsTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Plan would be to ask for the path to the file you want to open here
+		// this.addCommand({
+		// id: 'edit-in-neovim-open',
+		// name: 'Open In Neovim',
+		// editorCallback: (editor: Editor, view: MarkdownView) => {
+		// console.log(editor.getSelection());
+		// editor.replaceSelection('Sample Editor Command');
+		// }
+		// });
 	}
 
 	onunload() {
-
+		this.app.workspace.off("file-open", this.openInNeovimInstance)
 	}
 
 	async loadSettings() {
@@ -124,23 +82,7 @@ export default class EditInNeovim extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class EditInNeovimSettingsTab extends PluginSettingTab {
 	plugin: EditInNeovim;
 
 	constructor(app: App, plugin: EditInNeovim) {
@@ -154,13 +96,34 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Terminal')
+			.setDesc('Which terminal emulator should I try and use for the neovim instance?')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('E.g. alacritty, kitty, wezterm...')
+				.setValue(this.plugin.settings.terminal)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.terminal = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Neovim Server Location')
+			.setDesc('The Neovim instance will be spawned using --listen and needs a socket or IP:PORT (not sure if sockets work so use at your own risk)')
+			.addText(text => text
+				.setPlaceholder('127.0.0.1:2006')
+				.setValue(this.plugin.settings.listenOn)
+				.onChange(async value => {
+					this.plugin.settings.listenOn = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Open on startup')
+			.setDesc('Open the Neovim instance when Obsidian opens')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.openNeovimOnLoad)
+				.onChange(async value => {
+					this.plugin.settings.openNeovimOnLoad = value;
 					await this.plugin.saveSettings();
 				}));
 	}
